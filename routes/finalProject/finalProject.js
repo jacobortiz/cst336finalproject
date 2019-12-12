f29fcf3d95ad15a9df5bf465e2f68a6f55e38577
Apthox <kevin@apthos.net>
2019-12-11 16:12:48 -0800

    tournament post code

--- routes/finalProject/finalProject.js
+++ routes/finalProject/finalProject.js
@@ -133,4 +133,48 @@ router.get('/logout', function(req, res) {
     });
 });
 
+router.post('/create_tournament', function(req, res) {
+
+    username = "kevin1";
+
+    const connection = mysql.createConnection({
+        host: 'ui0tj7jn8pyv9lp6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'u4iixpff4n2b1uam',
+        password: 'gszyw5nfp2os51lq',
+        database: 'c2cyppf6xaxjv2wy'
+    });
+
+    connection.connect();
+
+    let successful = false;
+    let message = '';
+
+    date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+
+    connection.query(
+        'INSERT INTO tournament(username, title, levels, created , zip) VALUES (?, ?, ?, ?, ?);', 
+        [username, req.body.title, req.body.levels, date, req.body.zip], 
+        (error, results, fields) => {
+            if (error) {
+                res.json({
+                    successful: false,
+                    message: 'could not insert into tournament table!'
+                });
+            }
+
+            console.log("Tournament Created!");
+
+            console.log(req.body.matches);
+            connection.query(
+                ''
+            );
+        }
+    );
+
+    
+    
+    connection.end();
+
+});
+
 module.exports = router;
